60250c3ebf13bbfc6fe317b30724639b1fa1fa3c
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2024-01-18 15:32:25 +0000

    jan-first-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName] = useState("");
+  function paraChange(e){
+    setName("Hello " + e.target.value+"!")
+    if(e.target.value==""){
+      setName("");
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+       <h1>Enter Your Name</h1>
+       <input type="text" onChange={paraChange}></input>
+       <p>{name}</p>
     </div>
   )
 }
